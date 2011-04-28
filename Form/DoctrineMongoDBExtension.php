29260bb2a5578c38eaeda00811edb58001db04ed
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-04-28 15:06:43 -0700

    added document and collection data transformers

---
+++ Form/DoctrineMongoDBExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\DoctrineMongoDBBundle\Form;
+
+use Symfony\Component\Form\AbstractExtension;
+
+/**
+ * Form extension.
+ *
+ * @author Kris Wallsmith <kris@symfony.com>
+ */
+class DoctrineMongoDBExtension extends AbstractExtension
+{
+}
