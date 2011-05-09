08bc269335b14da59ffb66b7cbe98ad4eda461e2
ornicar <thibault.duplessis@gmail.com>
2011-05-09 11:58:39 -0700

    Remove unneeded loc from tests bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,7 +3,6 @@
 require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespace('Symfony\\Bundle\\DoctrineMongoDBBundle\\Tests', __DIR__.'/../../../..');
 $loader->registerNamespace('Symfony\\Bundle\\DoctrineMongoDBBundle', __DIR__.'/../../../..');
 $loader->registerNamespace('Symfony\\Tests', $_SERVER['SYMFONY_TESTS']);
 $loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
