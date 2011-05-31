b3579b6bca3370a7c120cf8dd81ae135721fb945
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-05-31 16:32:22 -0700

    fixed test suite bootstrap autoload of bundle classes so it works when not installed in a PSR-0 directory structure
    partial revert of 95a482729292f9fef1cbf31c2e5153ca1c9a5302

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,10 +3,17 @@
 require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespace('Symfony\\Bundle\\DoctrineMongoDBBundle', __DIR__.'/../../../..');
 $loader->registerNamespace('Symfony\\Tests', $_SERVER['SYMFONY_TESTS']);
 $loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
 $loader->registerNamespace('Doctrine\\ODM\\MongoDB', $_SERVER['DOCTRINE_MONGODB_ODM']);
 $loader->registerNamespace('Doctrine\\MongoDB', $_SERVER['DOCTRINE_MONGODB']);
 $loader->registerNamespace('Doctrine\\Common', $_SERVER['DOCTRINE_COMMON']);
 $loader->register();
+
+spl_autoload_register(function($class)
+{
+    if (0 === strpos($class, 'Symfony\\Bundle\\DoctrineMongoDBBundle\\') &&
+        file_exists($file = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php')) {
+        require_once $file;
+    }
+});
