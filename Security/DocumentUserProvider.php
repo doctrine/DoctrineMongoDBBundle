a777b11b05268d59334433dac71a78846f905a51
Jeremy Mikola <jmikola@gmail.com>
2011-11-16 17:43:51 -0500

    Add DocumentManager type-hinting to DocumentUserProvider constructor

--- Security/DocumentUserProvider.php
+++ Security/DocumentUserProvider.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Bundle\DoctrineMongoDBBundle\Security;
 
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
@@ -22,15 +23,15 @@ class DocumentUserProvider implements UserProviderInterface
     protected $repository;
     protected $property;
 
-    public function __construct($em, $class, $property = null)
+    public function __construct(DocumentManager $dm, $class, $property = null)
     {
         $this->class = $class;
 
         if (false !== strpos($this->class, ':')) {
-            $this->class = $em->getClassMetadata($class)->getName();
+            $this->class = $dm->getClassMetadata($class)->getName();
         }
 
-        $this->repository = $em->getRepository($class);
+        $this->repository = $dm->getRepository($class);
         $this->property = $property;
     }
 
