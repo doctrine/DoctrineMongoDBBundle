3e840da3efe41b641fa164baafadbd17dfc8b7d8
Bilal Amarni <bilal.amarni@gmail.com>
2012-05-10 22:33:51 +0200

    fixed tests annotation loader

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,17 @@
 <?php
 
-if (!@include __DIR__ . '/../vendor/.composer/autoload.php') {
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+if (!$loader = @include __DIR__ . '/../vendor/.composer/autoload.php') {
     die("You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
 php composer.phar install
 ");
 }
+
+AnnotationRegistry::registerLoader(function($class) use ($loader) {
+    $loader->loadClass($class);
+    return class_exists($class, false);
+});
+
+AnnotationRegistry::registerFile(__DIR__ . '/../vendor/doctrine/mongodb-odm/lib/Doctrine/ODM/MongoDB/Mapping/Annotations/DoctrineAnnotations.php');
