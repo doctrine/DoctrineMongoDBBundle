65c4fd917e4dfef22fb784ecac9ad33c659b0b9e
Bilal Amarni <bilal.amarni@gmail.com>
2012-05-10 18:22:31 +0200

    using composer for tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,27 +1,19 @@
 <?php
 
-/*
- * This file is part of the Doctrine MongoDBBundle
- *
- * The code was originally distributed inside the Symfony framework.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- * (c) Doctrine Project
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-    require_once $file;
+if (!@include __DIR__ . '/../vendor/.composer/autoload.php') {
+    die("You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+");
 }
 
-register_shutdown_function(function() {
-    try {
-        $mongo = new Mongo();
-        $mongo->doctrine->drop();
-    } catch (\MongoException $e) {
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'Doctrine\\Bundle\\MongoDBBundle')) {
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
+        return true;
     }
 });
