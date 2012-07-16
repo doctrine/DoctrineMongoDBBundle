84bfe3ca51d19898b28b07a279466839618b0e21
Jeremy Mikola <jmikola@gmail.com>
2012-07-16 17:53:59 -0400

    [Command] Expose the ODM's update schema command
    This command is used purely for updating indexes without collection-wide drop/create commands.
    
    See: doctrine/mongodb-odm#360

---
+++ Command/UpdateSchemaDoctrineODMCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Bundle\DoctrineMongoDBBundle\Command;
+
+use Doctrine\ODM\MongoDB\Tools\Console\Command\Schema\UpdateCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command to update the database schema for a set of classes based on their
+ * mappings.
+ */
+class UpdateSchemaDoctrineODMCommand extends UpdateCommand
+{
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setName('doctrine:mongodb:schema:update')
+            ->addOption('dm', null, InputOption::VALUE_REQUIRED, 'The document manager to use for this command.')
+            ->setHelp(<<<EOT
+The <info>doctrine:mongodb:schema:drop</info> command updates the default document manager's schema:
+
+  <info>./app/console doctrine:mongodb:schema:update</info>
+
+You can also optionally specify the name of a document manager to update the schema for:
+
+  <info>./app/console doctrine:mongodb:schema:update --dm=default</info>
+EOT
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        DoctrineODMCommand::setApplicationDocumentManager($this->getApplication(), $input->getOption('dm'));
+
+        parent::execute($input, $output);
+    }
+}
