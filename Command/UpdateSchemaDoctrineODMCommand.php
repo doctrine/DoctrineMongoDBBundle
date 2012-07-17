0e01caf18817b225a76e2c1616959d5a4127e327
Chris Jones <leeked@gmail.com>
2012-07-17 01:48:10 -0300

    Fixing namespace

--- Command/UpdateSchemaDoctrineODMCommand.php
+++ Command/UpdateSchemaDoctrineODMCommand.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Bundle\DoctrineMongoDBBundle\Command;
+namespace Doctrine\Bundle\DoctrineMongoDBBundle\Command;
 
 use Doctrine\ODM\MongoDB\Tools\Console\Command\Schema\UpdateCommand;
 use Symfony\Component\Console\Input\InputInterface;
