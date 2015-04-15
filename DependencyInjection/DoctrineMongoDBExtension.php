c6e4e36ce31c8b26515112e26d48330c24941288
Sullivan SENECHAL <sullivan@nexylan.com>
2015-04-15 13:42:10 +0200

    Fix deprecated Factory definition

--- DependencyInjection/DoctrineMongoDBExtension.php
+++ DependencyInjection/DoctrineMongoDBExtension.php
@@ -232,8 +232,12 @@ class DoctrineMongoDBExtension extends AbstractDoctrineExtension
             new Reference(sprintf('doctrine_mongodb.odm.%s_connection.event_manager', $connectionName)),
         );
         $odmDmDef = new Definition('%doctrine_mongodb.odm.document_manager.class%', $odmDmArgs);
-        $odmDmDef->setFactoryClass('%doctrine_mongodb.odm.document_manager.class%');
-        $odmDmDef->setFactoryMethod('create');
+        if (method_exists($odmDmDef, 'setFactory')) {
+            $odmDmDef->setFactory(array('%doctrine_mongodb.odm.document_manager.class%', 'create'));
+        } else {
+            $odmDmDef->setFactoryClass('%doctrine_mongodb.odm.document_manager.class%');
+            $odmDmDef->setFactoryMethod('create');
+        }
         $odmDmDef->addTag('doctrine_mongodb.odm.document_manager');
 
         $container
