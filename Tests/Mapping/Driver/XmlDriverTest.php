2c658c2a2f3ef083df0b41dacc618354c667cad9
Catalin Ciobanu <caciobanu@gmail.com>
2016-04-06 09:27:33 +0300

    Revert "Remove tests for deprecated classes"
    This reverts commit 953fad5eb3ad80daa00efc9e282f39b0a3ff08d6.

---
+++ Tests/Mapping/Driver/XmlDriverTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Doctrine MongoDBBundle
+ *
+ * The code was originally distributed inside the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Doctrine Project
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Doctrine\Bundle\MongoDBBundle\Tests\Mapping\Driver;
+
+use Doctrine\Bundle\MongoDBBundle\Mapping\Driver\XmlDriver;
+
+class XmlDriverTest extends AbstractDriverTest
+{
+    protected function getFileExtension()
+    {
+        return '.mongodb.xml';
+    }
+
+    protected function getFixtureDir()
+    {
+        return __DIR__ . '/Fixtures/xml';
+    }
+
+    protected function getDriver(array $prefixes = array())
+    {
+        return new XmlDriver($prefixes, $this->getFileExtension());
+    }
+}
