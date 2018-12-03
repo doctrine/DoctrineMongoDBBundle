4e49e5cd131835460fe24163dbfeb48347248393
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2018-12-03 16:02:41 +0100

    Fix #507 Symfony 4.2 deprecation
    https://github.com/doctrine/DoctrineMongoDBBundle/issues/507
    
    Not sure how far the BC goes, so if this will be for 4.0 or 3.5?

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -34,8 +34,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('doctrine_mongodb');
+        $treeBuilder = new TreeBuilder('doctrine_mongodb');
+        $rootNode = $treeBuilder->getRootNode();
 
         $this->addDocumentManagersSection($rootNode);
         $this->addConnectionsSection($rootNode);
