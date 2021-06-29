97b3078903a06f2d81aa71ca66451207700965d9
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2021-06-29 22:04:18 +0200

    Update tests

--- Tests/DataCollector/CommandDataCollectorTest.php
+++ Tests/DataCollector/CommandDataCollectorTest.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace Doctrine\Bundle\MongoDBBundle\Tests\DataCollector;
 
 use Doctrine\Bundle\MongoDBBundle\DataCollector\CommandDataCollector;
+use Doctrine\Bundle\MongoDBBundle\Tests\Fixtures\Form\Category;
 use Doctrine\Bundle\MongoDBBundle\Tests\TestCase;
-use Doctrine\ODM\MongoDB\APM\Command;
 use Doctrine\ODM\MongoDB\APM\CommandLogger;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,39 +16,52 @@ use function count;
 
 class CommandDataCollectorTest extends TestCase
 {
+    /** @var CommandLogger */
+    private $commandLogger;
+    /** @var DocumentManager */
+    private $dm;
+
+    protected function setUp(): void
+    {
+        $this->dm = TestCase::createTestDocumentManager();
+
+        $this->commandLogger = new CommandLogger();
+        $this->commandLogger->register();
+
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->commandLogger->unregister();
+
+        $this->dm->getDocumentCollection(Category::class)->drop();
+
+        parent::tearDown();
+    }
+
     public function testCollector(): void
     {
-        $commandLogger = $this->createCommandLogger([
-            $this->createCommand(['first' => 'firstCommand'], 100),
-            $this->createCommand(['second' => 'secondCommand'], 200),
-        ]);
+        $category = new Category('one');
 
-        $collector = new CommandDataCollector($commandLogger);
+        $this->dm->persist($category);
+        $this->dm->flush();
+
+        $this->dm->remove($category);
+        $this->dm->flush();
+
+        $this->dm->getRepository(Category::class)->findAll();
+
+        $collector = new CommandDataCollector($this->commandLogger);
         $collector->collect(new Request(), new Response());
 
-        self::assertSame(300, $collector->getTime());
-        self::assertSame(2, $collector->getCommandCount());
-        self::assertSame(100, $collector->getCommands()[0]['durationMicros']);
-        self::assertSame('firstCommand', $collector->getCommands()[0]['command']->first);
-        self::assertSame(200, $collector->getCommands()[1]['durationMicros']);
-        self::assertSame('secondCommand', $collector->getCommands()[1]['command']->second);
-    }
-
-    private function createCommandLogger(array $commands) : CommandLogger
-    {
-        $commandLoggerMock = $this->createMock(CommandLogger::class);
-        $commandLoggerMock->method('count')->willReturn(count($commands));
-        $commandLoggerMock->method('getAll')->willReturn($commands);
-
-        return $commandLoggerMock;
-    }
-
-    private function createCommand(array $command, int $durationMicros) : Command
-    {
-        $commandMock = $this->createMock(Command::class);
-        $commandMock->method('getCommand')->willReturn((object) $command);
-        $commandMock->method('getDurationMicros')->willReturn($durationMicros);
-
-        return $commandMock;
+        self::assertSame(3, $collector->getCommandCount());
+        self::assertGreaterThan(0, $collector->getTime());
+        self::assertSame('Category', $collector->getCommands()[0]['command']->insert);
+        self::assertGreaterThan(0, $collector->getCommands()[0]['durationMicros']);
+        self::assertSame('Category', $collector->getCommands()[1]['command']->delete);
+        self::assertGreaterThan(0, $collector->getCommands()[1]['durationMicros']);
+        self::assertSame('Category', $collector->getCommands()[2]['command']->find);
+        self::assertGreaterThan(0, $collector->getCommands()[2]['durationMicros']);
     }
 }
