bd64cd00f593cb98ad332c550507478046ada4bd
Ion Bazan <ion.bazan@gmail.com>
2021-10-14 13:36:48 +0800

    support Unique constraint as attribute

--- Validator/Constraints/Unique.php
+++ Validator/Constraints/Unique.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace Doctrine\Bundle\MongoDBBundle\Validator\Constraints;
 
+use Attribute;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Constraint for the unique document validator
  *
  * @Annotation
+ * @Target({"CLASS", "ANNOTATION"})
  */
+#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
 class Unique extends UniqueEntity
 {
     /** @var string */
