b9f23af4d4432dcc929b687f24c136b096d55615
Ion Bazan <ion.bazan@gmail.com>
2021-10-16 12:54:08 +0800

    add tests

---
+++ Tests/DependencyInjection/Fixtures/Bundles/AttributesBundle/AttributesBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMongoDBBundle\Tests\DependencyInjection\Fixtures\Bundles\AttributesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AttributesBundle extends Bundle
+{
+}
---
+++ Tests/DependencyInjection/Fixtures/Bundles/AttributesBundle/Document/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMongoDBBundle\Tests\DependencyInjection\Fixtures\Bundles\AttributesBundle\Document;
+
+class Test
+{
+}
