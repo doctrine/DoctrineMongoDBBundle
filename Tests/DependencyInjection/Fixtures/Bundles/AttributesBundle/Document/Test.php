cd5c344226ad5660da88e9a1dcd8e3d3503e3c04
Fran Moreno <franmomu@gmail.com>
2021-11-22 11:18:52 +0100

    Add missing mapping (#714)

--- Tests/DependencyInjection/Fixtures/Bundles/AttributesBundle/Document/Test.php
+++ Tests/DependencyInjection/Fixtures/Bundles/AttributesBundle/Document/Test.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace DoctrineMongoDBBundle\Tests\DependencyInjection\Fixtures\Bundles\AttributesBundle\Document;
 
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+#[MongoDB\Document]
 class Test
 {
 }
