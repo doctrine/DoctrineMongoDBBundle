b28dde19c1291462c3e790c55a394f3518b8b3aa
Jérôme Tamarelle <jerome@tamarelle.net>
2023-12-09 18:33:25 +0100

    Remove deprecated tail-cursor command and base class methods (#795)

--- Command/DoctrineODMCommand.php
+++ Command/DoctrineODMCommand.php
@@ -6,22 +6,8 @@ namespace Doctrine\Bundle\MongoDBBundle\Command;
 
 use Doctrine\Bundle\MongoDBBundle\ManagerRegistry;
 use Doctrine\ODM\MongoDB\Tools\Console\Helper\DocumentManagerHelper;
-use Doctrine\Persistence\ObjectManager;
-use InvalidArgumentException;
-use LogicException;
-use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-use function assert;
-use function sprintf;
-use function str_replace;
-use function strtolower;
-use function trigger_deprecation;
-
-use const DIRECTORY_SEPARATOR;
 
 /**
  * Base class for Doctrine ODM console commands to extend.
@@ -30,59 +16,9 @@ use const DIRECTORY_SEPARATOR;
  */
 abstract class DoctrineODMCommand extends Command
 {
-    /** @var ContainerInterface|null */
-    protected $container;
-
-    private ?ManagerRegistry $managerRegistry;
-
-    public function __construct(?ManagerRegistry $registry = null)
+    public function __construct(private ManagerRegistry $registry)
     {
         parent::__construct();
-
-        $this->managerRegistry = $registry;
-    }
-
-    /** @deprecated since version 4.4 */
-    public function setContainer(?ContainerInterface $container = null)
-    {
-        trigger_deprecation(
-            'doctrine/mongodb-odm-bundle',
-            '4.4',
-            'The "%s" method is deprecated and will be dropped in DoctrineMongoDBBundle 5.0.',
-            __METHOD__
-        );
-
-        $this->container = $container;
-    }
-
-    /**
-     * @deprecated since version 4.4
-     *
-     * @return ContainerInterface
-     *
-     * @throws LogicException
-     */
-    protected function getContainer()
-    {
-        trigger_deprecation(
-            'doctrine/mongodb-odm-bundle',
-            '4.4',
-            'The "%s" method is deprecated and will be dropped in DoctrineMongoDBBundle 5.0.',
-            __METHOD__
-        );
-
-        if ($this->container === null) {
-            $application = $this->getApplication();
-            if ($application === null) {
-                throw new LogicException('The container cannot be retrieved as the application instance is not yet set.');
-            }
-
-            assert($application instanceof Application);
-
-            $this->container = $application->getKernel()->getContainer();
-        }
-
-        return $this->container;
     }
 
     /** @param string $dmName */
@@ -94,101 +30,12 @@ abstract class DoctrineODMCommand extends Command
     }
 
     /**
-     * @deprecated since version 4.4
-     *
-     * @return ObjectManager[]
-     */
-    protected function getDoctrineDocumentManagers()
-    {
-        trigger_deprecation(
-            'doctrine/mongodb-odm-bundle',
-            '4.4',
-            'The "%s" method is deprecated and will be dropped in DoctrineMongoDBBundle 5.0.',
-            __METHOD__
-        );
-
-        return $this->getManagerRegistry()->getManagers();
-    }
-
-    /**
      * @internal
      *
      * @return ManagerRegistry
      */
     protected function getManagerRegistry()
     {
-        if ($this->managerRegistry === null) {
-            $this->managerRegistry = $this->container->get('doctrine_mongodb');
-            assert($this->managerRegistry instanceof ManagerRegistry);
-        }
-
-        return $this->managerRegistry;
-    }
-
-    /**
-     * @deprecated since version 4.4
-     *
-     * @param string $bundleName
-     *
-     * @return Bundle
-     */
-    protected function findBundle($bundleName)
-    {
-        trigger_deprecation(
-            'doctrine/mongodb-odm-bundle',
-            '4.4',
-            'The "%s" method is deprecated and will be dropped in DoctrineMongoDBBundle 5.0.',
-            __METHOD__
-        );
-
-        $foundBundle = false;
-
-        $application = $this->getApplication();
-
-        assert($application instanceof Application);
-
-        foreach ($application->getKernel()->getBundles() as $bundle) {
-            assert($bundle instanceof Bundle);
-            if (strtolower($bundleName) === strtolower($bundle->getName())) {
-                $foundBundle = $bundle;
-
-                break;
-            }
-        }
-
-        if (! $foundBundle) {
-            throw new InvalidArgumentException('No bundle ' . $bundleName . ' was found.');
-        }
-
-        return $foundBundle;
-    }
-
-    /**
-     * Transform classname to a path $foundBundle substract it to get the destination
-     *
-     * @deprecated since version 4.4
-     *
-     * @param Bundle $bundle
-     *
-     * @return string
-     */
-    protected function findBasePathForBundle($bundle)
-    {
-        trigger_deprecation(
-            'doctrine/mongodb-odm-bundle',
-            '4.4',
-            'The "%s" method is deprecated and will be dropped in DoctrineMongoDBBundle 5.0.',
-            __METHOD__
-        );
-
-        $path        = str_replace('\\', DIRECTORY_SEPARATOR, $bundle->getNamespace());
-        $search      = str_replace('\\', DIRECTORY_SEPARATOR, $bundle->getPath());
-        $destination = str_replace(DIRECTORY_SEPARATOR . $path, '', $search, $c);
-
-        if ($c !== 1) {
-            throw new RuntimeException(sprintf('Can\'t find base path for bundle (path: "%s", destination: "%s").', $path, $destination));
-        }
-
-        return $destination;
+        return $this->registry;
     }
 }
