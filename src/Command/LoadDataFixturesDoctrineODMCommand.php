e0f25872225ffe54a0dfb009fecaa5a7f83a7fb6
Jérôme Tamarelle <jerome@tamarelle.net>
2025-07-01 06:54:31 +0200

    Add purge-with-delete option to the fixtures:load command

--- src/Command/LoadDataFixturesDoctrineODMCommand.php
+++ src/Command/LoadDataFixturesDoctrineODMCommand.php
@@ -8,6 +8,7 @@ use Doctrine\Bundle\MongoDBBundle\Loader\SymfonyFixturesLoaderInterface;
 use Doctrine\Bundle\MongoDBBundle\ManagerRegistry;
 use Doctrine\Common\DataFixtures\Executor\MongoDBExecutor;
 use Doctrine\Common\DataFixtures\Purger\MongoDBPurger;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Psr\Log\AbstractLogger;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -15,7 +16,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use function assert;
 use function implode;
+use function method_exists;
 use function sprintf;
 
 /**
@@ -38,6 +41,7 @@ final class LoadDataFixturesDoctrineODMCommand extends DoctrineODMCommand
             ->addOption('group', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Only load fixtures that belong to this group (use with --services)')
             ->addOption('append', null, InputOption::VALUE_NONE, 'Append the data fixtures instead of flushing the database first.')
             ->addOption('dm', null, InputOption::VALUE_REQUIRED, 'The document manager to use for this command.')
+            ->addOption('purge-with-delete', null, InputOption::VALUE_NONE, 'Use deleteMany() to purge the collections instead of dropping them. This is useful to keep the collections, their metadata and their indexes, but it is slower than dropping them.')
             ->setHelp(<<<'EOT'
 The <info>doctrine:mongodb:fixtures:load</info> command loads data fixtures from your application:
 
@@ -50,6 +54,10 @@ If you want to append the fixtures instead of flushing the database first you ca
 You can also choose to load only fixtures that live in a certain group:
 
     <info>php %command.full_name%</info> <comment>--group=group1</comment>
+
+If the collection uses search indexes or encryption, you can use the <info>--purge-with-delete</info> option to keep the collections instead of dropping them when purging the database:
+
+  <info>php %command.full_name%</info> --purge-with-delete
 EOT
         );
     }
@@ -57,9 +65,17 @@ EOT
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $dm = $this->getManagerRegistry()->getManager($input->getOption('dm'));
+        assert($dm instanceof DocumentManager);
+
         $ui = new SymfonyStyle($input, $output);
 
-        if ($input->isInteractive() && ! $input->getOption('append')) {
+        if ($input->getOption('purge-with-delete')) {
+            if ($input->getOption('append')) {
+                $ui->error('The --purge-with-delete option cannot be used with the --append option.');
+
+                return self::INVALID;
+            }
+        } elseif ($input->isInteractive() && ! $input->getOption('append')) {
             $helper   = $this->getHelper('question');
             $question = new ConfirmationQuestion('Careful, database will be purged. Do you want to continue (y/N) ?', false);
 
@@ -82,7 +98,17 @@ EOT
             return 1;
         }
 
-        $purger   = new MongoDBPurger($dm);
+        $purger = new MongoDBPurger($dm);
+        if ($input->getOption('purge-with-delete')) {
+            if (! method_exists($purger, 'setPurgeMode')) {
+                $ui->error('The --purge-with-delete option requires doctrine/data-fixtures >= 2.1.0.');
+
+                return self::INVALID;
+            }
+
+            $purger->setPurgeMode(MongoDBPurger::PURGE_MODE_DELETE);
+        }
+
         $executor = new MongoDBExecutor($dm, $purger);
 
         $executor->setLogger(new class ($output) extends AbstractLogger {
