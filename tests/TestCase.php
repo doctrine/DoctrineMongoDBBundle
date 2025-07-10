9b30ccb7654718b9f429562fa8cce7d28682c3f7
Jérôme Tamarelle <jerome@tamarelle.net>
2025-07-10 11:17:08 +0200

    Enable client configuration for tests - partial

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,9 +7,11 @@ namespace Doctrine\Bundle\MongoDBBundle\Tests;
 use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Mapping\Driver\AttributeDriver;
+use MongoDB\Client;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
+use function getenv;
 use function sys_get_temp_dir;
 
 class TestCase extends BaseTestCase
@@ -25,7 +27,8 @@ class TestCase extends BaseTestCase
         $config->setHydratorNamespace('SymfonyTests\Doctrine');
         $config->setMetadataDriverImpl(new AttributeDriver($paths));
         $config->setMetadataCache(new ArrayAdapter());
+        $uri = getenv('DOCTRINE_MONGODB_SERVER') ?: 'mongodb://localhost:27017';
 
-        return DocumentManager::create(null, $config);
+        return DocumentManager::create(new Client($uri), $config);
     }
 }
