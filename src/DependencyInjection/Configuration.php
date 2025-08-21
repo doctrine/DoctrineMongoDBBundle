cf11f812824ef7eb63d99a9db923a8e0078508f6
Jérôme Tamarelle <jerome@tamarelle.net>
2025-08-21 17:54:47 +0200

    Revert extraOptions config to attributes for string and boolean options

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -12,7 +12,6 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 use function count;
-use function explode;
 use function in_array;
 use function is_array;
 use function is_string;
