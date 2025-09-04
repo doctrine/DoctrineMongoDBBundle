f3baab4919bd5746984e9e17b86427ee2a316b4c
Antoine Bluchet <soyuka@users.noreply.github.com>
2025-09-04 20:51:31 +0200

    Fix `#[Unique]` validation attribute with new `identifierFieldNames` parameter (#918)
    Use conditional declaration of the class Unique to override the UniqueEntity::$service default value respecting the property type introduced in Symfony 7.0
    
    https://github.com/symfony/symfony/commit/7ea24615d7b6f46a2af42cf860cd5f74f38eb7ad#diff-580bed00c9b884da104fb61d62ee451c36ef1ec4936c5a55b68aa4bd182c701b
    
    ---
    
    Co-authored-by: Jérôme Tamarelle <jerome@tamarelle.net>

--- tests/Validator/Constraints/UniqueTest.php
+++ tests/Validator/Constraints/UniqueTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Doctrine\Bundle\MongoDBBundle\Tests\Validator\Constraints;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Doctrine\Bundle\MongoDBBundle\Validator\Constraints\Unique;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
@@ -23,11 +25,71 @@ final class UniqueTest extends TestCase
         self::assertInstanceOf(Unique::class, $constraint);
         self::assertSame(['email'], $constraint->fields);
         self::assertSame('doctrine_odm.mongodb.unique', $constraint->validatedBy());
+        self::assertNull($constraint->em);
+    }
+
+    public function testWithGroups(): void
+    {
+        $metadata = new ClassMetadata(UniqueDocumentWithGroups::class);
+
+        $loader = new AttributeLoader();
+        self::assertTrue($loader->loadClassMetadata($metadata));
+
+        [$constraint] = $metadata->getConstraints();
+        self::assertInstanceOf(Unique::class, $constraint);
+        self::assertSame(['email'], (array) $constraint->fields);
+        self::assertSame(['group1'], $constraint->groups);
+        self::assertSame('doctrine_odm.mongodb.unique', $constraint->validatedBy());
+        self::assertNull($constraint->em);
+    }
+
+    public function testWithIdentifierFieldNames(): void
+    {
+        if (! InstalledVersions::satisfies(new VersionParser(), 'symfony/doctrine-bridge', '>= 7.3')) {
+            self::markTestSkipped('Requires symfony/doctrine-bridge 7.3 or higher, with $identifierFieldNames field');
+        }
+
+        $metadata = new ClassMetadata(UniqueDocumentWithIdentifierFieldNames::class);
+
+        $loader = new AttributeLoader();
+
+        self::assertTrue($loader->loadClassMetadata($metadata));
+
+        [$constraint] = $metadata->getConstraints();
+        self::assertInstanceOf(Unique::class, $constraint);
+        self::assertSame(['email'], $constraint->fields);
+        self::assertSame('Custom message', $constraint->message);
+        self::assertSame('custom_em', $constraint->em);
+        self::assertFalse($constraint->ignoreNull);
+        self::assertSame(['id', 'name'], $constraint->identifierFieldNames);
+        self::assertSame(['group1', 'group2'], $constraint->groups);
+        self::assertSame('doctrine_odm.mongodb.unique', $constraint->validatedBy());
     }
 }
 
 #[Unique(['email'])]
 class UniqueDocumentDummyOne
 {
-    private string $email;
+    public string $email;
+}
+
+#[Unique(fields: 'email', groups: ['group1'])]
+class UniqueDocumentWithGroups
+{
+    public string $email;
+}
+
+#[Unique(
+    ['email'],
+    'Custom message',
+    em: 'custom_em',
+    ignoreNull: false,
+    identifierFieldNames: ['id', 'name'],
+    groups: ['group1', 'group2'],
+)]
+class UniqueDocumentWithIdentifierFieldNames
+{
+    public string $name;
+
+    public string $email;
 }
