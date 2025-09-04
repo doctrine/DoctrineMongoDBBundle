3df8a63aecf3273b5c999e61e724e9cbeb3dcc5c
Jérôme Tamarelle <jerome@tamarelle.net>
2025-09-04 15:55:45 +0200

    Redefining a property in a trait with a different default value is not allowed

--- src/Validator/Constraints/Unique.php
+++ src/Validator/Constraints/Unique.php
@@ -5,16 +5,37 @@ declare(strict_types=1);
 namespace Doctrine\Bundle\MongoDBBundle\Validator\Constraints;
 
 use Attribute;
+use ReflectionProperty;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
-/**
- * Constraint for the unique document validator
- *
- * @Annotation
- * @Target({"CLASS", "ANNOTATION"})
- */
-#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
-class Unique extends UniqueEntity
-{
-    use UniqueInternalCompatibilityTrait;
+// @phpcsSuppress PSR1.Classes.ClassDeclaration.MultipleClasses
+if ((new ReflectionProperty(UniqueEntity::class, 'service'))->hasType()) {
+    /**
+     * Constraint for the unique document validator
+     *
+     * @Annotation
+     * @Target({"CLASS", "ANNOTATION"})
+     */
+    #[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+    class Unique extends UniqueEntity
+    {
+        public string $service = 'doctrine_odm.mongodb.unique';
+    }
+} else {
+    // Compatibility for symfony/doctrine-bridge < 7.0
+    /**
+     * Constraint for the unique document validator
+     *
+     * @Annotation
+     * @Target({"CLASS", "ANNOTATION"})
+     */
+    #[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+    class Unique extends UniqueEntity
+    {
+        /**
+         * @phpstan-ignore missingType.property
+         * @var string $service
+         */
+        public $service = 'doctrine_odm.mongodb.unique';
+    }
 }
