f54758781a73b15246e11586ebd5b533e05a87e4
Jérôme Tamarelle <jerome@tamarelle.net>
2025-12-10 17:58:20 +0100

    Fix test configuration for native lazy objects and Symfony 8 (#965)
    * Fix configuration for native lazy objects
    
    * Skip XML config tests with Symfony 8+
    
    * Make matrix parameters order consistent for Job title

--- tests/DependencyInjection/XmlMongoDBExtensionTest.php
+++ tests/DependencyInjection/XmlMongoDBExtensionTest.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Doctrine\Bundle\MongoDBBundle\Tests\DependencyInjection;
 
+use PHPUnit\Framework\Attributes\RequiresMethod;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
+/** XML Support was removed from Symfony 8.0 */
+#[RequiresMethod(XmlFileLoader::class, 'load')]
 class XmlMongoDBExtensionTest extends AbstractMongoDBExtensionTestCase
 {
     protected function loadFromFile(ContainerBuilder $container, string $file): void
